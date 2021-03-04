025feb4565b5c099485d74ffd18e31c483f86197
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-04 20:36:23 +0900

    react-flux

--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -33,10 +33,10 @@ export default class Nav extends React.Component {
           <div class={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
               <li class={featuredClass}>
-                <Link to="/" onClick={this.toggleCollapse.bind(this)}>Featured</Link>
+                <Link to="/" onClick={this.toggleCollapse.bind(this)}>Todos</Link>
               </li>
               <li class={archivesClass}>
-              <Link to="/archives/news?date=today&filter=none" onClick={this.toggleCollapse.bind(this)}>Archives</Link>
+              <Link to="/favorites" onClick={this.toggleCollapse.bind(this)}>Favorites</Link>
               </li>
               <li class={settingsClass}>
                 <Link to="/settings" onClick={this.toggleCollapse.bind(this)}>Settings</Link>
